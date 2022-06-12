024421cd94f8253274684ae75429fa394da86e15
ivanlon <ivanlon@foxmail.com>
2022-06-13 01:37:36 +0800

    update README

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # pixSync
 
-Sync your bookmarked illusts to local
+Sync your bookmarked illusts to local disk (WIP)
+
+## Usage
+
+### Get your `refresh token`  by [`get-pixivpy-token`](https://github.com/eggplants/get-pixivpy-token)  
+0. Make sure Chrome installed
+1. Install `get-pixivpy-token` by `pip install gppt`  
+2. `gppt login`, a Chrome window will be opened
+3. Enter your pixiv username and password then click login
+4. The Chrome window will close automatically, and you will see something like below in your terminal
+    ```bash
+    [+]: Success!
+    access_token: ***
+    refresh_token: ***
+    expires_in: 3600
+    ```
+
+### Get your `PHPSESSID` 
+1. Run Chrome or Firefox, open `Web Developer Tools` by press `F12`, switch to `Network` tab
+2. Login [pixiv](https://www.pixiv.net)
+3. Click any of the sessions that appear
+4. Find `PHPSESSID` in the `Cookie` field of `Request Headers`  
+   ![](images/PHPSESSID.png)
+
+### Config
+1. Rename `config.json.sample` to `config.json` and place your tokens and `PHPSESSID` in it
+2. Edit `config.yaml` as needed
+
+### Run
+`python main.py`  
+For periodic synchronization, please use `crontab` or something
+
+## Functions and todos
+### Functions
++ [x] Download **ALL** your bookmarked illusts and mangas (unsupport ugoira)
+
+### WIP
++ [ ] Download only new bookmarked illusts
++ [ ] Automatic rename
+
+### TODOs
++ [ ] Download and transcoding ugoira
++ [ ] Integrate `get-pixivpy-token`
++ [ ] Get `PHPSESSID` automatically by `selenium`
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import os
-from typing import *
-import pixivpy3 as pixiv
 import json
 import logging
-import yaml
-import time
-import sys
+import os
 import pickle
+import sys
+import time
+from typing import *
+
+import pixivpy3 as pixiv
+import yaml
 
 logger = logging.Logger("pixSync", level=logging.DEBUG)
 log_handler = logging.StreamHandler(sys.stdout)
@@ -15,25 +16,29 @@ log_handler.setFormatter(logging.Formatter("[%(levelname)s]\t%(message)s"))
 logger.addHandler(log_handler)
 
 
-def load_token(token_path:str="token.json") -> Dict[str, str]:
+def load_token(token_path: str = "token.json") -> Dict[str, str]:
     with open(token_path, 'r', encoding="utf8") as fp:
         data = json.load(fp)
     return data
 
 
-def load_config(config_path:str="config.yaml") -> Dict[str, str]:
+def load_config(config_path: str = "config.yaml") -> Dict[str, str]:
     with open(config_path, 'r', encoding="utf8") as fp:
         config = yaml.load(fp, Loader=yaml.Loader)
     return config
 
 
-def get_downloaded_illusts(metapath:str="bookmark.json") -> Set[int]:
-    with open(metapath, 'r', encoding="utf8") as fp:
-        data = json.load(fp)
-    return set([int(illust["id"]) for illust in data])
+def get_downloaded_illusts(metapath: str = "bookmark.json") -> Set[int]:
+    try:
+        with open(metapath, 'r', encoding="utf8") as fp:
+            data = json.load(fp)
+    except FileNotFoundError:
+        return set()
+    else:
+        return set([int(illust["id"]) for illust in data])
 
 
-def download(api:pixiv.AppPixivAPI, illusts:List, save_dir:str) -> Dict:
+def download(api: pixiv.AppPixivAPI, illusts: List, save_dir: str) -> Dict:
     result = {
         "success": [],
         "fail": []
@@ -42,7 +47,8 @@ def download(api:pixiv.AppPixivAPI, illusts:List, save_dir:str) -> Dict:
         try:
             if illust["type"] in {"illust", "manga"}:
                 if illust["page_count"] == 1:
-                    api.download(illust["meta_single_page"]["original_image_url"], path=save_dir)
+                    api.download(illust["meta_single_page"]
+                                 ["original_image_url"], path=save_dir)
                 else:
                     for url in illust["meta_pages"]:
                         url = url["image_urls"]["original"]
@@ -57,17 +63,17 @@ def download(api:pixiv.AppPixivAPI, illusts:List, save_dir:str) -> Dict:
             result["fail"].append((illust["id"], str(e)))
     return result
 
-def main(api:pixiv.AppPixivAPI=None):
+
+def main():
     config = load_config()
     token = load_token()
     logger.info(str(config))
     logger.info(str(token))
-    
-    if api is None:
-        api = pixiv.AppPixivAPI(cookies={
-            "PHPSESSID": token["sessid"]
-        })
-        logger.info("init new aapi")
+
+    api = pixiv.AppPixivAPI(cookies={
+        "PHPSESSID": token["sessid"]
+    })
+    logger.info("init new aapi")
 
     for i in range(config["max_retry"]):
         try:
@@ -80,12 +86,13 @@ def main(api:pixiv.AppPixivAPI=None):
     else:
         logger.error("auth failed, please retry")
         exit(255)
-        
+
     illusts = []
     for restrict in config["restrict"]:
         mbi = None
-        while True:  
-            res = api.user_bookmarks_illust(me["user"]["id"], restrict=restrict, max_bookmark_id=mbi)
+        while True:
+            res = api.user_bookmarks_illust(
+                me["user"]["id"], restrict=restrict, max_bookmark_id=mbi)
             illusts.extend(res["illusts"])
             logger.info(f"get {len(illusts)} illusts")
             if res["next_url"]:
@@ -93,29 +100,19 @@ def main(api:pixiv.AppPixivAPI=None):
                 time.sleep(0.5)
             else:
                 break
-    
+
     downloaded_illusts = get_downloaded_illusts()
-    new_illusts = [illust for illust in illusts if int(illust["id"]) not in downloaded_illusts]
+    new_illusts = [illust for illust in illusts if int(
+        illust["id"]) not in downloaded_illusts]
     logger.info(f"{len(new_illusts)} new illusts")
-    
+
     os.makedirs(config["save_path"], exist_ok=True)
     result = download(api, new_illusts, config["save_path"])
-    logger.info(f"finish: {len(result['success'])} success, {len(result['fail'])} fail:")
+    logger.info(
+        f"finish: {len(result['success'])} success, {len(result['fail'])} fail:")
     if result["fail"]:
         logger.info('\n'.join([f"{id} - {exc}" for id, exc in result['fail']]))
-    
-    return api
-    
+
+
 if __name__ == "__main__":
-    SESS_PATH = "api.session"
-    try:
-        with open(SESS_PATH, "rb") as fp:
-            api = pickle.load(fp)
-    except FileNotFoundError:
-        api = None
-        
-    api = main(api=api)
-    
-    if isinstance(api, pixiv.AppPixivAPI):
-        with open(SESS_PATH, "wb") as fp:
-            pickle.dump(api, fp)
+    api = main()
